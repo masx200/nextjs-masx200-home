3c0456dac0e46aeccae79e0745a931675b35349d
masx200 <masx200@qq.com>
2021-08-03 11:49:41 +0800

    lazy

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} loading="lazy" />
         </a>
       </footer>
     </div>
