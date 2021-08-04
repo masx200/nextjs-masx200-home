8b64088d5018e935afad1137f698f1c8ab203305
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-04 11:42:26 +0800

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import { PropsWithChildren } from "react";
-import Home from '../components/react-module-home'
+import Home from "../components/react-module-home";
 const index = (props: PropsWithChildren<{}>) => {
     console.log(props);
-    return <div><Home></Home></div>;
+    return (
+        <div>
+            <Home></Home>
+        </div>
+    );
 };
 export default index;
