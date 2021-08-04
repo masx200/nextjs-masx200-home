85326e8a3c2798612b3cedbb0c450d4c10351ae7
masx200 <masx200@qq.com>
2021-08-04 22:04:18 +0800

    优化

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 // @ts-check
 ("use strict");
-const TerserPlugin = require("terser-webpack-plugin");
+
 /**
  * @type {Partial< import('next/dist/next-server/server/config').NextConfig>}
  **/
@@ -27,35 +27,6 @@ const nextConfig = {
         const ftcwp = new ForkTsCheckerWebpackPlugin();
         config.plugins = [...config.plugins, ftcwp];
 
-        const terserplugin = new TerserPlugin({
-            terserOptions: {
-                ecma: 5,
-                parse: {
-                    ecma: 8,
-                },
-                compress: {
-                    warnings: !1,
-                    comparisons: !1,
-                    inline: 2,
-                    drop_console: true,
-                    drop_debugger: true,
-                    pure_funcs: ["console.log"],
-                },
-                mangle: {
-                    safari10: !0,
-                },
-                output: {
-                    ecma: 5,
-                    comments: !1,
-                    ascii_only: !0,
-                },
-            },
-            parallel: !0,
-        });
-        config.optimization.minimizer = [
-            terserplugin,
-            ...config.optimization.minimizer,
-        ];
         if (!dev) {
             config.optimization.minimize = true;
         }
