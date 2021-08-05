b4897089b453fde85b1eb6a8fec21242c537a414
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-05 15:41:24 +0800

    marked Renderer  DOMPurify

--- components/markdown-react/getrenderedmarkdown.tsx
+++ components/markdown-react/getrenderedmarkdown.tsx
@@ -3,11 +3,35 @@ import marked from "marked";
 // @ts-ignore
 import hljs from "../../src/assetsjs/highlight.min.js";
 import { fetchtext } from "./fetchtext";
+import createDOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
 
+const DOMPurify = (() => {
+    const window = new JSDOM("").window;
+    //@ts-ignore
+    const DOMPurify = createDOMPurify(window);
+    return DOMPurify;
+})();
+
+// console.log(DOMPurify);
 // console.log(hljs)
+const { Renderer } = marked;
+class myrenderer extends Renderer {
+    image(href: string | null, title: string | null, text: string): string {
+        console.log(href, title, text);
+        var result = super.image(href, title, text);
+        console.log(result);
+        if (result.startsWith("<img src=")) {
+            result = result.replace("<img src=", "<img loading='lazy' src=");
+        }
+        return result;
+    }
+}
 export async function getrenderedmarkdown(src: string) {
     const text = await fetchtext(src);
-    return marked(text, {
+
+    const dirty = marked(text, {
+        renderer: new myrenderer(),
         baseUrl: src,
         highlight(code, lang) {
             // console.log(code,lang)
@@ -19,4 +43,6 @@ export async function getrenderedmarkdown(src: string) {
                 : hljs.highlightAuto(code).value;
         },
     });
+    const clean = DOMPurify.sanitize(dirty);
+    return clean;
 }
