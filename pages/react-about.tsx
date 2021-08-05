b576d0c14cf8ae937fcca4e5fc6cacc4dc178ad6
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-05 20:39:25 +0800

    格式化

--- pages/react-about.tsx
+++ pages/react-about.tsx
@@ -5,7 +5,6 @@ function 获取当前时间() {
     return new Date().toString().slice(0, new Date().toString().indexOf("GMT"));
 }
 
-
 export default function about() {
     const [nowtime, setnowtime] = useState(获取当前时间());
     useEffect(function () //   componentWillMount() {}
