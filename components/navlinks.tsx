ae9da0e9dc3c9d18e46c5ff29e4fdac05113a7d5
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-05 18:02:46 +0800

    Update navlinks.tsx

--- components/navlinks.tsx
+++ components/navlinks.tsx
@@ -7,10 +7,10 @@ export const navlinks: Linktype[] = [
     { text: "花密网页版", href: { pathname: "/react-huami" } },
     { text: "rss阅读器", href: { pathname: "/react-rssreader" } },
 
-    { text: "圆周率计算多线程", href: { pathname: "/picalc" } },
-    { text: "JSfuck-and-hieroglyphy-Decoder", href: { pathname: "/decoder" } },
-    { text: "JSfuck-ENCODER", href: { pathname: "/jsfuck" } },
-    { text: "hieroglyphy-ENCODER", href: { pathname: "/hieroglyphy" } },
+    // { text: "圆周率计算多线程", href: { pathname: "/picalc" } },
+    // { text: "JSfuck-and-hieroglyphy-Decoder", href: { pathname: "/decoder" } },
+    // { text: "JSfuck-ENCODER", href: { pathname: "/jsfuck" } },
+    // { text: "hieroglyphy-ENCODER", href: { pathname: "/hieroglyphy" } },
     {
         text: "极速零配置的单页面 web 应用打包工具",
         href: { pathname: "/webpack-react-vue-spa-awesome-config" },
