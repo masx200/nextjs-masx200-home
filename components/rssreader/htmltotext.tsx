28fd6e957ecb5655652c46c2ae5c378116b5e8eb
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-06 11:33:50 +0800

    Update htmltotext.tsx

--- components/rssreader/htmltotext.tsx
+++ components/rssreader/htmltotext.tsx
@@ -4,7 +4,16 @@ export function htmltotext(description: string): string {
     //alert(description)
     const body = document.implementation.createHTMLDocument("").body;
     body.innerHTML = description;
-
+    if ("undefined" === typeof body.innerText) {
+        Object.defineProperty(body, "innerText", {
+            get() {
+                return this.textContent;
+            },
+            set(value) {
+                this.textContent = value;
+            },
+        });
+    }
     const text = body.innerText;
 
     // console.log("text", text);
