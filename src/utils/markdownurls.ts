96f9eeb1893a453d2f9d8788971b39a6dea62fc5
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-16 08:13:42 +0800

    Update markdownurls.ts

--- src/utils/markdownurls.ts
+++ src/utils/markdownurls.ts
@@ -1,15 +1,8 @@
-export default {
-    // "vue-simple-global-state-store-manager":
-    //     "https://cdn.jsdelivr.net/gh/masx200/vue-simple-global-state-store-manager@latest/README.md",
-
+ export default {
     "webpack-react-vue-spa-awesome-config":
         "https://cdn.jsdelivr.net/gh/masx200/webpack-react-vue-spa-awesome-config/README.md",
     "excellent-vscode-extensions-for-javascript":
         "https://cdn.jsdelivr.net/gh/masx200/excellent-vscode-extensions-for-javascript/README.md",
-    // "react-simple-global-state-store-hook":
-    //     "https://cdn.jsdelivr.net/gh/masx200/react-simple-global-state-store-hook@latest/README.md",
-    // importcjsamdumd:
-    //     "https://cdn.jsdelivr.net/gh/masx200/importcjsamdumd@latest/README.md",
     "masx200-github-io":
-        "https://cdn.jsdelivr.net/gh/masx200/masx200-github-io/README.md",
+        "https://masx200.github.io/masx200-github-io/README.md",
 };
