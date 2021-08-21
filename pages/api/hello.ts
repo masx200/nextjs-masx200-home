8f0ea664ad194f401cdfe24f8d162e9b356cf181
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-21 20:01:05 +0800

    删除无用文件

--- pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    const { url, method } = req;
-    res.statusCode = 200;
-    res.json(Object.assign({ url, method }, req.headers));
-};
