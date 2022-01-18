21bc4d89e4ffbfda30b6ba0f58bc5ed928377e79
masx200 <34191203+masx200@users.noreply.github.com>
2022-01-18 14:16:12 +0800

    修复marked和图片loading=lazy

--- components/markdown-react/getrenderedmarkdown.ts
+++ components/markdown-react/getrenderedmarkdown.ts
@@ -1,5 +1,6 @@
 "use strict";
-import marked from "marked";
+// @ts-ignore
+import { marked } from "marked";
 // @ts-ignore
 import hljs from "../../src/assetsjs/highlight.min.js";
 import { fetchtext } from "./fetchtext";
@@ -7,21 +8,25 @@ import createDOMPurify from "dompurify";
 import { JSDOM } from "jsdom";
 
 const DOMPurify = createpurify();
+if (typeof document === "undefined") {
+    const dom = new JSDOM(``, {});
+    Reflect.set(global, "document", dom.window.document);
+}
 
 // console.log(DOMPurify);
 // console.log(hljs)
-const { Renderer } = marked;
-class myrenderer extends Renderer {
-    image(href: string | null, title: string | null, text: string): string {
-        // console.log(href, title, text);
-        var result = super.image(href, title, text);
-        // console.log(result);
-        if (result.startsWith("<img src=")) {
-            result = result.replace("<img src=", "<img loading='lazy' src=");
-        }
-        return result;
-    }
-}
+// const { Renderer } = marked;
+// class myrenderer extends Renderer {
+//     image(href: string | null, title: string | null, text: string): string {
+//         // console.log(href, title, text);
+//         var result = super.image(href, title, text);
+//         // console.log(result);
+//         if (result.startsWith("<img src=")) {
+//             result = result.replace("<img src=", "<img loading='lazy' src=");
+//         }
+//         return result;
+//     }
+// }
 function createpurify() {
     const window = new JSDOM("").window;
     //@ts-ignore
@@ -29,11 +34,11 @@ function createpurify() {
     return DOMPurify;
 }
 
-export async function getrenderedmarkdown(src: string) {
+export async function getrenderedmarkdown(src: string): Promise<string> {
     const text = await fetchtext(src);
 
     const dirty = marked(text, {
-        renderer: new myrenderer(),
+        // renderer: new myrenderer(),
         baseUrl: src,
         highlight(code, lang) {
             // console.log(code,lang)
@@ -46,5 +51,23 @@ export async function getrenderedmarkdown(src: string) {
         },
     });
     const clean = DOMPurify.sanitize(dirty);
-    return clean;
+    const element = htmltoelement(`<div>${clean}</div>`);
+    imgsetlazy(element);
+    const html = element.innerHTML;
+    return html;
+}
+export function htmltoelement(html: string): HTMLElement {
+    const container = document.createElement("div");
+    container.innerHTML = html;
+    const ret = container.childNodes[0] as HTMLElement;
+    if (!ret) {
+        throw Error("html element not found");
+    }
+    return ret;
+}
+export function imgsetlazy(tmpdoc: HTMLElement) {
+    Array.from(tmpdoc.querySelectorAll("img")).forEach((e) => {
+        e.loading = "lazy";
+        e.setAttribute("loading", "lazy");
+    });
 }
