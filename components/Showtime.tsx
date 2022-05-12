f3fffaccb3d300810981724bd0d6517a541ca486
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-12 19:30:06 +0800

    修复

--- components/Showtime.tsx
+++ components/Showtime.tsx
@@ -3,7 +3,7 @@ import React, { memo, useEffect, useState } from "react";
 import { 获取当前时间 } from "./获取当前时间";
 
 export const Showtime = memo(function Showtime() {
-    const [nowtime, setnowtime] = useState('Thu May 12 2022 19:23:20');
+    const [nowtime, setnowtime] = useState("Thu May 12 2022 19:23:20");
     useEffect(function () {
         // refreshall();
         // document.title = "React router App-" + "about";
