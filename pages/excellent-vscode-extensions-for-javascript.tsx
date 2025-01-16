4c41096f450520217d594b08f055d93ce98aca0a
masx200 <masx200@qq.com>
2025-01-16 19:33:25 +0800

    rssxml

--- pages/excellent-vscode-extensions-for-javascript.tsx
+++ pages/excellent-vscode-extensions-for-javascript.tsx
@@ -1,16 +1,16 @@
 import { GetStaticProps } from "next";
 import { memo } from "react";
 import Home, {
-  markurl,
+    markurl,
 } from "../components/excellent-vscode-extensions-for-javascript";
 import { getrenderedmarkdown } from "../components/markdown-react/getrenderedmarkdown";
 const reacthome = memo(({ markdown }: { markdown: string }) => {
-  return <Home markdown={markdown}></Home>;
+    return <Home markdown={markdown}></Home>;
 });
 export default reacthome;
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  // console.log(context);
-  const markdown = await getrenderedmarkdown(markurl);
-  return { /* revalidate: 86400, */ props: { markdown } };
+    // console.log(context);
+    const markdown = await getrenderedmarkdown(markurl);
+    return { /* revalidate: 86400, */ props: { markdown } };
 };
