889d64758563f19c9162adad7e341511d7aa10b1
masx200 <masx200@qq.com>
2025-01-23 00:51:36 +0800

    陶辉

--- components/rssreader/rssfeedxml.ts
+++ components/rssreader/rssfeedxml.ts
@@ -11,5 +11,6 @@ export default {
     酷安网: "https://weibrss.oneoo.info/rss/1746523974",
     coderjia: "https://www.coderjia.cn/rss.xml",
     appscross: "https://appscross.com/rss",
-vumstar:"https://masx200.github.io/nextjs-masx200-home/rss/vumstar.rss"
+    vumstar: "https://masx200.github.io/nextjs-masx200-home/rss/vumstar.rss",
+陶辉:"https://weibrss.oneoo.info/rss/1871821935"
 };
