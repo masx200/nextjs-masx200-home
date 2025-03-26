39930fd1a804ce5e2b3cf4a8eb22d66621fc6355
masx200 <masx200@qq.com>
2025-03-27 00:48:04 +0800

    refactor(components): 优化 CSS 链接和 RSS 源
    - 为 CSS 链接添加 type="text/css" 属性
    - 更新 CSS 文件路径为相对路径
    - 修改 RSS 源地址，统一为 RSS XML 文件
    - 优化部分 RSS 源地址格式

--- components/rssreader/rssfeedxml.ts
+++ components/rssreader/rssfeedxml.ts
@@ -1,5 +1,5 @@
 export default {
-    最后三人: "https://bk.197501.xyz/rss.xml",
+    最后三人: "https://masx200.github.io/masx200-github-io/rss/最后三人/rss.xml",
     tmtpost: "https://www.tmtpost.com/feed/",
     iplaysoft: "https://www.iplaysoft.com/feed/",
     landiannews: "https://www.landiannews.com/feed/",
@@ -19,6 +19,7 @@ export default {
     sukka: "https://masx200.github.io/masx200-github-io/rss/sukka/rss.xml",
     运维世界: "https://www.ywsj.cf/rss.xml",
     云匿博客: "https://www.yunieebk.com/rss.xml",
+   
     星尘幻梦:
-        "https://masx200.github.io/masx200-github-io/rss/星尘幻梦/feed.xml",
+        "https://masx200.github.io/masx200-github-io/rss/星尘幻梦/rss.xml",
 };
